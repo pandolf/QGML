31885f27c57423a01fe0985d2996428b1f668bd9
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2017-06-13 15:30:28 +0200

    not used anymore

--- analysis/trainQGML.py
+++ analysis/trainQGML.py
@@ -1,8 +1,8 @@
 import numpy as np
-#import ROOT
 import root_numpy as rn
 import pandas as pd
 from sklearn import svm
+from sklearn.model_selection import train_test_split
 
 
 a = rn.root2array('qgMiniTuple_pandas.root', 'qgLite')
@@ -18,4 +18,10 @@ clf = svm.SVC(gamma=0.001, C=100.)
 
 clf.fit(X_train, y_train)
 
-y_pred = clf.predict(X_test)
+y_pred = pd.Series(clf.predict(X_test))
+
+correct = (y_pred == y_test.reset_index()['isQuark'])
+totcorr = sum(correct)
+acc = totcorr/float(len(y_pred))
+
+print( "Accuracy = %d / %d = %.2f %%" % (totcorr,len(y_test),100.*acc) )
