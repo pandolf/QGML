47efbcd692be4ee1065df1f459fb98696a51f812
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2017-08-25 17:51:28 +0200

    two different files

--- analysis/createPDFs_mixed.cpp
+++ analysis/createPDFs_mixed.cpp
@@ -7,32 +7,36 @@
 
 
 
-void createPDFs( TFile* file, TTree* tree, const std::string& name, float ptMin, float ptMax );
+void createPDFs( TTree* tree, const std::string& name, float ptMin, float ptMax );
 
 
 
 int main() {
 
-  TFile* fileDY = TFile::Open("treelite_DYJetsToLL_M50_LO.root");
-  TTree* treeDY = (TTree*)fileDY->Get("tlite");
+  TChain* treeDY = new TChain("tlite");
+  treeDY->Add("treeLite_DYJetsToLL_M50_HT100to200.root");
+  treeDY->Add("treeLite_DYJetsToLL_M50_HT200to400.root");
+  treeDY->Add("treeLite_DYJetsToLL_M50_HT400to600.root");
+  treeDY->Add("treeLite_DYJetsToLL_M50_HT600to800.root");
 
   TChain* treeQCD = new TChain("tlite");
-  treeQCD->Add("treelite_QCD_Pt50to80.root");
-  treeQCD->Add("treelite_QCD_Pt80to120.root");
-  treeQCD->Add("treelite_QCD_Pt120to170.root");
-  treeQCD->Add("treelite_QCD_Pt170to300.root");
+  treeQCD->Add("treeLite_QCD_Pt50to80.root");
+  treeQCD->Add("treeLite_QCD_Pt80to120.root");
+  treeQCD->Add("treeLite_QCD_Pt120to170.root");
+  treeQCD->Add("treeLite_QCD_Pt170to300.root");
+  //treeQCD->Add("treeLite_QCD_Pt300to470.root");
 
-  TFile* pdfFile = TFile::Open("pdfsMixed.root", "recreate");
-
-  createPDFs( pdfFile, treeDY ,  "dy", 100., 120. );
-  createPDFs( pdfFile, treeQCD, "qcd", 100., 120. );
+  createPDFs( treeDY ,  "dy", 100., 120. );
+  createPDFs( treeQCD, "qcd", 100., 120. );
 
   return 0;
 
 }
 
 
-void createPDFs( TFile* file, TTree* tree, const std::string& name, float ptMin, float ptMax ) {
+void createPDFs( TTree* tree, const std::string& name, float ptMin, float ptMax ) {
+
+  std::cout << "-> Creating PDFs for: " << name << std::endl;
 
   float weight;
   float pt;
@@ -49,19 +53,24 @@ void createPDFs( TFile* file, TTree* tree, const std::string& name, float ptMin,
   tree->SetBranchAddress( "mult", &mult );
 
 
+  TFile* file = TFile::Open(Form("pdfsMixed_%s.root", name.c_str()), "recreate");
   file->cd();
   
-  TH1D* h1_ptd   = new TH1D(Form("%s_pdf_ptd", name.c_str()), "", 30, 0., 1.0001 );
+  TH1D* h1_ptd   = new TH1D("pdf_ptd", "", 30, 0., 1.0001 );
   h1_ptd->Sumw2();
-  TH1D* h1_axis2 = new TH1D(Form("%s_pdf_axis2", name.c_str()), "", 30, 0., 10. );
+  TH1D* h1_axis2 = new TH1D("pdf_axis2", "", 30, 0., 10. );
   h1_axis2->Sumw2();
-  TH1D* h1_mult  = new TH1D(Form("%s_pdf_mult", name.c_str()), "", 50, 0., 50. );
+  TH1D* h1_mult  = new TH1D("pdf_mult", "", 50, 0., 50. );
   h1_mult->Sumw2();
 
   int nentries = tree->GetEntries();
 
 
   for( unsigned iEntry=0; iEntry<nentries; ++iEntry ) {
+
+    if( iEntry % 100000 == 0 ) std::cout << "Entry: " << iEntry << " / " << nentries << std::endl;
+
+    tree->GetEntry(iEntry);
 
     if( pt<ptMin || pt>ptMax ) continue;
 
@@ -77,5 +86,6 @@ void createPDFs( TFile* file, TTree* tree, const std::string& name, float ptMin,
   h1_axis2->Write();
   h1_mult ->Write();
 
+  file->Close();
 
 }
