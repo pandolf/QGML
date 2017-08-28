bc3b120f1f3283faa49e1fbb4e695f39d93afeb4
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2017-08-28 16:39:24 +0200

    added other vars

--- analysis/drawPDFmixed.cpp
+++ analysis/drawPDFmixed.cpp
@@ -13,7 +13,7 @@
 
 
 
-void drawVar( const std::string& outdir, TTree* tree, const std::string& var, int nBins, float xMin, float xMax, const std::string& axisName, int legendQuadrant );
+void drawVar( const std::string& outdir, TTree* tree, const std::string& var, const std::string& sel, int nBins, float xMin, float xMax, const std::string& axisName, int legendQuadrant );
 
 
 
@@ -68,7 +68,9 @@ int main( int argc, char* argv[] ) {
     //tree->Add("treeLite_QCD_Pt3200.root");
   }
   
-  drawVar( outdir, tree, "pt", 50, 100., 200., "p_{T} [GeV]", 2 );
+  drawVar( outdir, tree, "pt" , ""                  , 50, 100.,   200., "p_{T} [GeV]", 2 );
+  drawVar( outdir, tree, "eta", "pt>120. && pt<200.", 50,  -2.,     2., "#eta"       , 0 );
+  drawVar( outdir, tree, "qgl", "pt>120. && pt<200.", 50,   0., 1.0001, "QG LD"      , 0 );
 
 
 
@@ -76,7 +78,7 @@ int main( int argc, char* argv[] ) {
 }
 
 
-void drawVar( const std::string& outdir, TTree* tree, const std::string& var, int nBins, float xMin, float xMax, const std::string& axisName, int legendQuadrant ) {
+void drawVar( const std::string& outdir, TTree* tree, const std::string& var, const std::string& sel, int nBins, float xMin, float xMax, const std::string& axisName, int legendQuadrant ) {
 
   std::string hname_q( Form("h1_q_%s", var.c_str()) );
   std::string hname_g( Form("h1_g_%s", var.c_str()) );
@@ -86,8 +88,8 @@ void drawVar( const std::string& outdir, TTree* tree, const std::string& var, in
   TH1D* h1_g = new TH1D( hname_g.c_str(), "", nBins, xMin, xMax );
   h1_g->Sumw2();
 
-  tree->Project( hname_q.c_str(), var.c_str(), "w*(abs(partonId)<5)" );
-  tree->Project( hname_g.c_str(), var.c_str(), "w*(partonId==21)" );
+  tree->Project( hname_q.c_str(), var.c_str(), Form("w*(abs(partonId)<5 && %s)", sel.c_str()) );
+  tree->Project( hname_g.c_str(), var.c_str(), Form("w*(partonId==21    && %s)", sel.c_str()) );
 
   float hint_q = h1_q->Integral();
   float hint_g = h1_g->Integral();
@@ -113,7 +115,7 @@ void drawVar( const std::string& outdir, TTree* tree, const std::string& var, in
   stack->Add( h1_q );
   stack->Add( h1_g );
 
-  TH2D* h2_axes = new TH2D( "axes", "", 10, xMin, xMax, 10, 0., 1.3*yMax );
+  TH2D* h2_axes = new TH2D( "axes", "", 10, xMin, xMax, 10, 0., 1.35*yMax );
   h2_axes->SetXTitle( axisName.c_str() );
   h2_axes->SetYTitle( "Normalized to Unity" );
 
@@ -123,13 +125,17 @@ void drawVar( const std::string& outdir, TTree* tree, const std::string& var, in
 
   h2_axes->Draw();
 
-  stack->Draw("same");
 
   float xMin_leg, xMax_leg, yMin_leg, yMax_leg;
 
   if( legendQuadrant==2 ) {
     xMin_leg = 0.67;
     xMax_leg = 0.9;
+    yMin_leg = 0.73;
+    yMax_leg = 0.9;
+  } else if( legendQuadrant==0 ) {
+    xMin_leg = 0.39;
+    xMax_leg = 0.61;
     yMin_leg = 0.73;
     yMax_leg = 0.9;
   }
@@ -140,6 +146,8 @@ void drawVar( const std::string& outdir, TTree* tree, const std::string& var, in
   legend->AddEntry( h1_q, "Quarks", "F" );
   legend->AddEntry( h1_g, "Gluons", "F" );
   legend->Draw("same");
+
+  stack->Draw("same");
 
   TPaveText* labelTop = QGMLCommon::get_labelTop();
   TPaveText* labelCMS = QGMLCommon::get_labelCMS();
