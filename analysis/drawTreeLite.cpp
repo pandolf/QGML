8ac928ea67977dd8801de984d28cc101c77bcaa6
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2017-09-01 16:38:12 +0200

    added qglM and roc, but still havent tested

--- analysis/drawTreeLite.cpp
+++ analysis/drawTreeLite.cpp
@@ -10,10 +10,12 @@
 #include "TH1D.h"
 #include "THStack.h"
 #include "TChain.h"
+#include "TGraph.h"
 
 
 
-void drawVar( const std::string& outdir, TTree* tree, const std::string& var, const std::string& sel, int nBins, float xMin, float xMax, const std::string& axisName, int legendQuadrant );
+void drawVar( const std::string& outdir, TTree* tree, const std::string& var, float ptMin, float ptMax, int nBins, float xMin, float xMax, const std::string& axisName, int legendQuadrant );
+void drawROC( const std::string& outdir, TTree* tree, float ptMin, float ptMax );
 
 
 
@@ -79,17 +81,18 @@ int main( int argc, char* argv[] ) {
     //tree->Add("treeLite_QCD_Pt3200.root");
   }
   
-  drawVar( outdir, tree, "pt" , ""                  , 50, 100.,   200., "p_{T} [GeV]", 2 );
-  drawVar( outdir, tree, "eta", "pt>120. && pt<200.", 50,  -2.,     2., "#eta"       , 0 );
-  drawVar( outdir, tree, "qgl", "pt>120. && pt<200.", 50,   0., 1.0001, "QG LD"      , 0 );
+  drawVar( outdir, tree, "pt"  ,  -1.,  -1., 50, 100.,   200., "p_{T} [GeV]"    , 2 );
+  drawVar( outdir, tree, "eta" , 120., 200., 50,  -2.,     2., "#eta"           , 0 );
+  drawVar( outdir, tree, "qgl" , 120., 200., 50,   0., 1.0001, "QG LD"          , 0 );
+  drawVar( outdir, tree, "qglM", 120., 200., 50,   0., 1.0001, "DY-QCD LD"      , 0 );
 
-
+  drawROC( outdir, tree, 120., 200. );
 
 
 }
 
 
-void drawVar( const std::string& outdir, TTree* tree, const std::string& var, const std::string& sel, int nBins, float xMin, float xMax, const std::string& axisName, int legendQuadrant ) {
+void drawVar( const std::string& outdir, TTree* tree, const std::string& var, float ptMin, float ptMax, int nBins, float xMin, float xMax, const std::string& axisName, int legendQuadrant ) {
 
   std::string hname_q( Form("h1_q_%s", var.c_str()) );
   std::string hname_g( Form("h1_g_%s", var.c_str()) );
@@ -102,10 +105,12 @@ void drawVar( const std::string& outdir, TTree* tree, const std::string& var, co
   TH1D* h1_u = new TH1D( hname_u.c_str(), "", nBins, xMin, xMax );
   h1_u->Sumw2();
 
-  if( sel!="" ) {
-    tree->Project( hname_q.c_str(), var.c_str(), Form("w*(abs(partonId)<5 && abs(partonId)>0 && %s)", sel.c_str()) );
-    tree->Project( hname_g.c_str(), var.c_str(), Form("w*(partonId==21                       && %s)", sel.c_str()) );
-    tree->Project( hname_u.c_str(), var.c_str(), Form("w*(partonId==0                        && %s)", sel.c_str()) );
+  if( ptMin>-1. && ptMax>-1. ) {
+    char sel[300];
+    sprintf( sel, " pt>%f && pt<%f", ptMin, ptMax );
+    tree->Project( hname_q.c_str(), var.c_str(), Form("w*(abs(partonId)<5 && abs(partonId)>0 && %s)", sel) );
+    tree->Project( hname_g.c_str(), var.c_str(), Form("w*(partonId==21                       && %s)", sel) );
+    tree->Project( hname_u.c_str(), var.c_str(), Form("w*(partonId==0                        && %s)", sel) );
   } else {
     tree->Project( hname_q.c_str(), var.c_str(), Form("w*(abs(partonId)<5 && abs(partonId)>0)") );
     tree->Project( hname_g.c_str(), var.c_str(), Form("w*(partonId==21                      )") );
@@ -173,6 +178,8 @@ void drawVar( const std::string& outdir, TTree* tree, const std::string& var, co
   TLegend* legend = new TLegend( xMin_leg, yMin_leg, xMax_leg, yMax_leg );
   legend->SetTextSize ( 0.038 );
   legend->SetFillColor( 0 );
+  if( ptMin>-1. && ptMax>-1. ) 
+    legend->SetHeader( Form("%.0f < p_{T} < %.0f GeV", ptMin, ptMax) );
   legend->AddEntry( h1_q, "Quarks", "F" );
   legend->AddEntry( h1_g, "Gluons", "F" );
   legend->AddEntry( h1_u, "Undefined", "F" );
@@ -199,3 +206,96 @@ void drawVar( const std::string& outdir, TTree* tree, const std::string& var, co
   delete stack;
 
 }
+
+
+void drawROC( const std::string& outdir, TTree* tree, float ptMin, float ptMax ) {
+
+  TH1D* h1_qgl_quark = new TH1D( "qgl_quark", "", 100, 0., 1.0001 );
+  h1_qgl_quark->Sumw2();
+  TH1D* h1_qgl_gluon = new TH1D( "qgl_gluon", "", 100, 0., 1.0001 );
+  h1_qgl_gluon->Sumw2();
+  TH1D* h1_qglM_quark = new TH1D( "qglM_quark", "", 100, 0., 1.0001 );
+  h1_qglM_quark->Sumw2();
+  TH1D* h1_qglM_gluon = new TH1D( "qglM_gluon", "", 100, 0., 1.0001 );
+  h1_qglM_gluon->Sumw2();
+
+
+  tree->Project( "qgl_quark" , "qgl" , Form("w*( abs(partonId)>0 && abs(partonId)<5 && pt>%.0f && pt<%.0f", ptMin, ptMax) );
+  tree->Project( "qgl_gluon" , "qgl" , Form("w*( partonId==21                       && pt>%.0f && pt<%.0f", ptMin, ptMax) );
+  tree->Project( "qglM_quark", "qglM", Form("w*( abs(partonId)>0 && abs(partonId)<5 && pt>%.0f && pt<%.0f", ptMin, ptMax) );
+  tree->Project( "qglM_gluon", "qglM", Form("w*( partonId==21                       && pt>%.0f && pt<%.0f", ptMin, ptMax) );
+
+
+  TGraph* gr_RoC_qgl  = new TGraph(0);
+  TGraph* gr_RoC_qglM = new TGraph(0);
+
+  int nbins = h1_qglM_quark->GetNbinsX();
+
+  for( unsigned int ibin=1; ibin<nbins+1; ++ibin ) {
+
+    float eff_q_qgl  = h1_qgl_quark ->Integral( nbins-ibin, nbins )/h1_qgl_quark ->Integral( 1, nbins );
+    float eff_g_qgl  = h1_qgl_gluon ->Integral( nbins-ibin, nbins )/h1_qgl_gluon ->Integral( 1, nbins );
+  
+    float eff_q_qglM = h1_qglM_quark->Integral( nbins-ibin, nbins )/h1_qglM_quark->Integral( 1, nbins );
+    float eff_g_qglM = h1_qglM_gluon->Integral( nbins-ibin, nbins )/h1_qglM_gluon->Integral( 1, nbins );
+  
+    gr_RoC_qglM->SetPoint( ibin-1, 1.-eff_g_qglM, eff_q_qglM );
+    gr_RoC_qgl ->SetPoint( ibin-1, 1.-eff_g_qgl , eff_q_qgl  );
+
+  }
+
+
+  gr_RoC_qglM->SetMarkerSize(1.3);
+  gr_RoC_qglM->SetMarkerStyle(24);
+  gr_RoC_qglM->SetMarkerColor(kRed+3);
+
+  gr_RoC_qgl->SetMarkerSize(1.3);
+  gr_RoC_qgl->SetMarkerStyle(21);
+  gr_RoC_qgl->SetMarkerColor(29);
+
+
+  TCanvas* c1 = new TCanvas("c1_roc", "", 600, 600);
+  c1->cd();
+
+  TH2D* h2_axes = new TH2D("axes_roc", "", 10, 0., 1.0001, 10, 0., 1.0001);
+  h2_axes->SetXTitle( "Gluon Jet Rejection" );
+  h2_axes->SetYTitle( "Quark Jet Efficiency" );
+
+  h2_axes->Draw();
+
+  TLine* diag = new TLine(0., 1., 1., 0.);
+  diag->Draw("same");
+
+
+  char legendTitle[300];
+  sprintf( legendTitle, "%.0f < p_{T} < %.0f GeV, |#eta| < 1.3", ptMin, ptMax );
+  TLegend* legend = new TLegend( 0.2, 0.2, 0.45, 0.45, legendTitle );
+  legend->SetFillColor(0);
+  legend->SetTextSize(0.04);
+  legend->AddEntry( gr_RoC_qgl , "Standard QGL"       , "P");
+  legend->AddEntry( gr_RoC_qglM, "QGL with Mixed PDFs", "P");
+  legend->Draw("same");
+
+
+  TPaveText* labelTop = QGMLCommon::get_labelTop();
+  TPaveText* labelCMS = QGMLCommon::get_labelCMS();
+
+  labelTop->Draw("same");
+  labelCMS->Draw("same");
+
+  
+  gr_RoC_qgl->Draw("p same");
+  gr_RoC_qglM->Draw("p same");
+
+  gPad->RedrawAxis();
+
+  c1->SaveAs( Form( "%s/RoC_pt%.0f_%.0f.eps", outdir.c_str(), ptMin, ptMax ) );
+  c1->SaveAs( Form( "%s/RoC_pt%.0f_%.0f.pdf", outdir.c_str(), ptMin, ptMax ) );
+
+  delete c1;
+  delete h2_axes;
+  delete legend;
+
+}
+
+
